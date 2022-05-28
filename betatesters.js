749a4b1e03b8295067d98cfba174aaa08a2a2eb6
Mahir Molai <mahir@molai.dev>
2022-05-28 14:55:01 +0500

    Add beta testers

--- betatesters.js
+++ betatesters.js
@@ -4,5 +4,7 @@ module.exports = [
     "mousemoosz@protonmail.com",
     "contact@mail.badboy.is-a.dev",
     "an4g7k@gmail.com",
-    "andrew@win11react.com"
+    "andrew@win11react.com",
+    "kumarjhaabhishek2002@gmail.com",
+    "327953@mariscollege.nl"
 ]
