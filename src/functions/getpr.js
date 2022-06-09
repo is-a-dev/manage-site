688816ab3fc744f5197497a00666c68e03e23a9a
andrewstech <info@andrewstech.me>
2022-06-09 22:07:48 +0100

    Update endpoint

--- src/functions/getpr.js
+++ src/functions/getpr.js
@@ -22,7 +22,7 @@ async function getpr() {
         const url = res.data[0].html_url
         //pr number is res.data[0].number
         window.open(url, '_blank', 'noopener,noreferrer');
-        fetch("https://api.is-a-dev.asylum-os.com/pr/" + res.data[0].number)
+        fetch("https://notify-api.is-a.dev/pr/" + res.data[0].number)
             .then((response) => response.json())
             .then((data) => console.log(data));
     })
