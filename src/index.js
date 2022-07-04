ad9467cc28931a83912e194435ada490effe5945
William Harrison <william@williamdavidharrison.com.au>
2022-07-05 07:22:30 +0800

    Prettier

--- src/index.js
+++ src/index.js
@@ -11,9 +11,8 @@ Sentry.init({
   dsn: "https://962651c0d4b74c5fbbeeb1c5e3553476@o1276241.ingest.sentry.io/6471661",
   integrations: [new BrowserTracing()],
 
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
+  // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+  // We recommend adjusting this value in production.
   tracesSampleRate: 1.0,
 });
 
@@ -28,7 +27,6 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// If you want to start measuring performance in your app, pass a function to log results (for example: reportWebVitals(console.log)) or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
