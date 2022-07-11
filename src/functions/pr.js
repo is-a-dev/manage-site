faab0f109ebc3ded963ba335ffc50c79c325ea67
andrewstech <andrew@win11react.com>
2022-07-11 23:03:53 +0100

    Update pr.js

--- src/functions/pr.js
+++ src/functions/pr.js
@@ -8,6 +8,7 @@ async function openPR(domain) {
   const ghtoken = token[0].toString();
   const user = token[1].toString();
   const repository = token[3].toString();
+  const beta-message = " Via Open Beta Program: https://open-beta.is-a.dev/"
 
   const octokit = new Octokit({
     auth: ghtoken,
@@ -15,8 +16,8 @@ async function openPR(domain) {
   const pr = await octokit.request("POST /repos/{owner}/{repo}/pulls", {
     owner: "is-a-dev",
     repo: repository,
-    title: "Add Subdomain:" + domain + " via Web App",
-    body: "Added the domain  " + domain + " to the list of domains",
+    title: "BETA" + "Add Subdomain:" + domain,
+    body: "Added the domain  " + domain + beta-message,
     head: user + ":main",
     base: "main",
   });
