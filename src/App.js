e31fdcd78e5e24da62e84049cb48dff5eb1c3def
Mahir Molai <mahir@molai.dev>
2022-08-02 22:45:04 +0500

    remove delete account

--- src/App.js
+++ src/App.js
@@ -126,9 +126,7 @@ if(name == null) {
 
       <div className="btnBox">
         <button onClick={() => auth.signOut()}>Sign Out</button>
-        <button className="btn-red" onClick={() => auth.currentUser.delete()}>
-                  Delete Account
-        </button>
+
       </div>
     </>
   );
