c18c1756ee11a252fbc10d4cd961e35edb248bfd
Mahir Molai <mahir@molai.dev>
2022-08-22 21:31:01 +0500

    this should work

--- src/App.js
+++ src/App.js
@@ -139,10 +139,10 @@ if(name == null) {
               <option value="URL">URL</option>
             </select>
           </div>
-        <label className="input-group" id="parentSub">
+        <div className="subdomainfield">
           <input type="text" minLength={3} id="subdomain" placeholder="Subdomain" {...register("subdomain", {required: true, max: 12})} />
           <span>.is-a.dev</span>
-        </label>
+        </div>
         <input type="text" id="value" placeholder="Record value" {...register("value", {required: true})} />
         <div className="btnBox">
           <button id="register" className="btn-submit" type="submit" >Register</button>
