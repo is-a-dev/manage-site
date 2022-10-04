77e69b42c9aa6c3eabbfb6de9de5bf3d9be54365
andrewstech <info@andrewstech.me>
2022-10-04 13:30:25 +0100

    start backend proccessing

--- src/functions/fork.js
+++ src/functions/fork.js
@@ -6,10 +6,10 @@ async function fork(token) {
   const headers = { 'x-gh-auth': token };
   fetch('https://register-api.is-a.dev/api/fork', { headers })
     .then(async (res) => {
-      if(res.responce && res.responce == "authorized") {
+      if(res.status && res.status == "200") {
         console.log("Forked!");
       } else {
-        alert("Error: " + res.responce);
+        alert("Error: " + "not a 200 status code");
       }
     });
 }
