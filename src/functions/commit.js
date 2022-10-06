92b91ae10709863f0ee16b025081713c08935f6c
andrewstech <info@andrewstech.me>
2022-10-06 17:14:21 +0100

    start backend proccessing

--- src/functions/commit.js
+++ src/functions/commit.js
@@ -9,45 +9,23 @@ async function commit(domain, FileContents) {
   const { Base64 } = require("js-base64");
   const token = Object.values(vars);
   const ghtoken = token[0].toString();
-  const user = token[1].toString();
-  const email = token[2].toString();
-  const repository = token[3].toString();
-  const contentEncoded = Base64.encode(FileContents);
 
-  const octokit = new Octokit({ auth: ghtoken });
-  let ChosenDomain = domain;
-  let type = ".json";
-  let fileName = ChosenDomain.concat(type);
-  
-  fetch(`https://api.github.com/repos/is-a-dev/register/contents/domains/${domain}.json`, {
-            method: "GET",
-            headers: {
-                "User-Agent": "mtgsquad"
-            }
-        }).then(async (res) => {
-            if(res.status && res.status == 404) {
-                const { data } = await octokit.repos.createOrUpdateFileContents({
-                  owner: user,
-                  repo: repository,
-                  path: "domains/" + fileName,
-                  message: "feat: Added domain programatically",
-                  content: contentEncoded,
-                  committer: {
-                    name: user,
-                    email: email,
-                  },
 
-                  author: {
-                    name: user,
-                    email: email,
-                  }
-                })
+  const requestOptions = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json', "x-gh-auth": ghtoken, "domain": domain },
+    body: FileContents
+  };
+  fetch('https://register.is-a.dev/api/commit', requestOptions)
+    .then(async (res) => {
+      if(res.status && res.status == "202") {
+        console.log("Committed!");
+        openPR(domain);
+      } else {
+        alert("Error: " + res.status);
+      }
+    });
+}
 
-                openPR(domain);
-                getpr();
-              } else {
-                alert("Domain already exists!");
-        }});
-  }
 
 export default commit;
