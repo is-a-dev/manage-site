39ce0464191ad5e68c7e8fa30d431e1fa657afd5
William Harrison <william@williamdavidharrison.com.au>
2023-01-28 16:09:44 +0800

    prettier + new commit message

--- src/functions/commit.js
+++ src/functions/commit.js
@@ -5,48 +5,54 @@ import openPR from "./pr";
 import getpr from "./getpr";
 
 async function commit(domain, FileContents) {
-  const { Octokit } = require("@octokit/rest");
-  const { Base64 } = require("js-base64");
-  const token = Object.values(vars);
-  const ghtoken = token[0].toString();
-  const user = token[1].toString();
-  const email = token[2].toString();
-  const repository = token[3].toString();
-  const contentEncoded = Base64.encode(FileContents);
+    const { Octokit } = require("@octokit/rest");
+    const { Base64 } = require("js-base64");
+    const token = Object.values(vars);
+    const ghtoken = token[0].toString();
+    const user = token[1].toString();
+    const email = token[2].toString();
+    const repository = token[3].toString();
+    const contentEncoded = Base64.encode(FileContents);
 
-  const octokit = new Octokit({ auth: ghtoken });
-  let ChosenDomain = domain;
-  let type = ".json";
-  let fileName = ChosenDomain.concat(type);
-  
-  fetch(`https://api.github.com/repos/is-a-dev/register/contents/domains/${domain}.json`, {
+    const stripExt = replace(/\.[^/.]+$/, "");
+
+    const octokit = new Octokit({ auth: ghtoken });
+    let ChosenDomain = domain;
+    let type = ".json";
+    let fileName = ChosenDomain.concat(type);
+
+    fetch(
+        `https://api.github.com/repos/is-a-dev/register/contents/domains/${domain}.json`,
+        {
             method: "GET",
             headers: {
-                "User-Agent": "mtgsquad"
-            }
-        }).then(async (res) => {
-            if(res.status && res.status == 404) {
-                const { data } = await octokit.repos.createOrUpdateFileContents({
-                  owner: user,
-                  repo: repository,
-                  path: "domains/" + fileName.toLowerCase(),
-                  message: `feat(domain): add ${fileName.toLowerCase()}`,
-                  content: contentEncoded,
-                  committer: {
+                "User-Agent": "mtgsquad",
+            },
+        }
+    ).then(async (res) => {
+        if (res.status && res.status == 404) {
+            const { data } = await octokit.repos.createOrUpdateFileContents({
+                owner: user,
+                repo: repository,
+                path: "domains/" + fileName.toLowerCase(),
+                message: `feat(domain): ${fileName.toLowerCase().stripExt()}.is-a.dev`,
+                content: contentEncoded,
+                committer: {
                     name: user,
                     email: email,
-                  },
-                  author: {
+                },
+                author: {
                     name: user,
                     email: email,
-                  }
-                })
+                },
+            });
 
-                openPR(domain);
-                getpr();
-              } else {
-                alert("Domain already exists!");
-        }});
-  }
+            openPR(domain);
+            getpr();
+        } else {
+            alert("Domain already exists!");
+        }
+    });
+}
 
 export default commit;
