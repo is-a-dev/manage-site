c93d6421de78f1b6bdedd65b82161fb8dc459d0b
William Harrison <william@williamdavidharrison.com.au>
2023-01-28 16:07:11 +0800

    prettier + update body

--- src/functions/pr.js
+++ src/functions/pr.js
@@ -2,27 +2,27 @@ import "@octokit/core";
 import vars from "../vars";
 
 async function openPR(domain) {
-  const { Octokit } = require("@octokit/core");
-  const { createTokenAuth } = require("@octokit/auth-token");
-  const token = Object.values(vars);
-  const ghtoken = token[0].toString();
-  const user = token[1].toString();
-  const repository = token[3].toString();
-  
-  const octokit = new Octokit({
-    auth: ghtoken,
-  })
+    const { Octokit } = require("@octokit/core");
+    const { createTokenAuth } = require("@octokit/auth-token");
+    const token = Object.values(vars);
+    const ghtoken = token[0].toString();
+    const user = token[1].toString();
+    const repository = token[3].toString();
 
-  const pr = await octokit.request("POST /repos/{owner}/{repo}/pulls", {
-    owner: "is-a-dev",
-    repo: repository,
-    title: `Register ${domain.toLowerCase().replace(/\.[^/.]+$/, "")}.is-a.dev`,
-    body: `Added \`${domain.toLowerCase().replace(/\.[^/.]+$/, "")}\` via the register website.`,
-    head: user + ":main",
-    base: "main",
-  });
+    const octokit = new Octokit({ auth: ghtoken });
 
-  return pr;
+    const stripExt = replace(/\.[^/.]+$/, "");
+
+    const pr = await octokit.request("POST /repos/{owner}/{repo}/pulls", {
+        owner: "is-a-dev",
+        repo: repository,
+        title: `Register ${domain.toLowerCase().stripExt()}.is-a.dev`,
+        body: `Added \`${domain.toLowerCase().stripExt()}\` via the registration website.`,
+        head: user + ":main",
+        base: "main",
+    });
+
+    return pr;
 }
 
 export default openPR;
