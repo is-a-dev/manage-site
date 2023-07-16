798c388d5a3247ffeac5d36ddcac058d78a94356
andrewstech <info@andrewstech.me>
2023-07-16 20:08:52 +0000

    Start on delete page

--- src/routes/domains/[domain]/+page.svelte
+++ src/routes/domains/[domain]/+page.svelte
@@ -6,6 +6,10 @@
 
 <h2 class="h2">{data.domain.name}.is-a.dev</h2>
 <br />
-<button class="btn variant-filled" on:click={() => goto(`/domains/${data.domain.name}/edit`)}>
-	Edit Record(s)
+<button class="btn variant-filled">
+	Edit Record(s)  - Coming Soon
+</button>
+<br />
+<button class="btn terminal-font variant-filled-error text-center textnexttext mt-4" on:click={() => goto(`/domains/${data.domain.name}/delete`)}>
+	Delete Domain
 </button>
