06df4d645becba7f934c36b1ba808d8a10a93ca5
daanschenkel <20974756+daanschenkel@users.noreply.github.com>
2023-07-21 21:04:15 +0200

    Add get domain api

---
+++ src/routes/api/domains/[domain]/get/+server.js
@@ -0,0 +1,8 @@
+import { DomainInfo } from '$lib/api.js'
+import { json } from '@sveltejs/kit';
+export async function GET({params}){
+    let domain = params.domain;
+    if(!domain) return json({error: 'No domain provided'}, 400);
+    let result = await DomainInfo(domain);
+    return json(result, 200);
+}
