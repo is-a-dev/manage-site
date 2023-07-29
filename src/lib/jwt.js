945da6b7e9581ef52e4842e59631747e76f376e9
daanschenkel <20974756+daanschenkel@users.noreply.github.com>
2023-07-29 13:40:42 +0200

    add "oauth"

--- src/lib/jwt.js
+++ src/lib/jwt.js
@@ -1,14 +1,18 @@
 import jsonwebtoken from 'jsonwebtoken';
-const { verify } = jsonwebtoken;
-import { env } from '$env/dynamic/private'; 
+const { verify, sign } = jsonwebtoken;
+import { env } from '$env/dynamic/private';
 
-export function getJWT(jwt){
-    //verify the jwt
-    let response = verify(jwt, env.JWT_SECRET, (err, response) => {
-        if(err){
-            return null;
-        }
-        return response;
-    });
-    return response || null;
+export function getJWT(jwt) {
+	//verify the jwt
+	let response = verify(jwt, env.JWT_SECRET, (err, response) => {
+		if (err) {
+			return null;
+		}
+		return response;
+	});
+	return response || null;
+}
+
+export function createJWT(data, secret = env.JWT_SECRET, options = {}) {
+	return sign(data, secret, options);
 }
--- src/routes/login/callback/+server.js
+++ src/routes/login/callback/+server.js
@@ -1,65 +1,71 @@
 import jsonwebtoken from 'jsonwebtoken';
 const { sign } = jsonwebtoken;
 import { redirect } from '@sveltejs/kit';
-import { env } from '$env/dynamic/private'; 
+import { env } from '$env/dynamic/private';
 
 export async function GET({ url, cookies }) {
-    let query = url.searchParams.get('code');
-    if (!query) return new Response('The code was not found in the request', { status: 400 })
-    
-    //authorize with github
-    let token = await fetch(`https://github.com/login/oauth/access_token?client_id=${env.GITHUB_CLIENT_ID}&client_secret=${env.GITHUB_CLIENT_SECRET}&code=${query}`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            Accept: "application/json",
-        },
-    }).then((res) => res.json());
-    if (!token.access_token) return new Response('The access token was not found in the response', { status: 400 })
-    
-    //get user data
-    let user = await fetch('https://api.github.com/user', {
-        headers: {
-            Authorization: `token ${token.access_token}`
-        }
-    }).then((res) => res.json());
-    if(user.message === 'Bad credentials'){
-        return new Response('The generated token was invalid', { status: 400 })
-    }
-    
-    //get emails
-    let emails = await fetch('https://api.github.com/user/emails', {
-        headers: {
-            Authorization: `token ${token.access_token}`
-        }
-    }).then((res) => res.json());
-    if(emails.message === 'Bad credentials'){
-        return new Response('The generated token was invalid', { status: 400 })
-    }
+	let query = url.searchParams.get('code');
+	if (!query) return new Response('The code was not found in the request', { status: 400 });
 
+	//authorize with github
+	let token = await fetch(
+		`https://github.com/login/oauth/access_token?client_id=${env.GITHUB_CLIENT_ID}&client_secret=${env.GITHUB_CLIENT_SECRET}&code=${query}`,
+		{
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				Accept: 'application/json'
+			}
+		}
+	).then((res) => res.json());
+	if (!token.access_token)
+		return new Response('The access token was not found in the response', { status: 400 });
 
-    //create a jwt
-    let jwt = sign({
-        user: user,
-        token: token.access_token,
-        emails: emails
-    }, env.JWT_SECRET, {
-        expiresIn: '8h'
-    });
+	//get user data
+	let user = await fetch('https://api.github.com/user', {
+		headers: {
+			Authorization: `token ${token.access_token}`
+		}
+	}).then((res) => res.json());
+	if (user.message === 'Bad credentials') {
+		return new Response('The generated token was invalid', { status: 400 });
+	}
 
-    //set the cookie
-    cookies.set('jwt', jwt, {
-        httpOnly: true,
-        path: '/',
-        maxAge: 60 * 60 * 8,
-        
-    });
-    cookies.set('welcome', true, {
-        httpOnly: false,
-        path: '/',
-        maxAge: 60 * 60 * 8,
-    });
+	//get emails
+	let emails = await fetch('https://api.github.com/user/emails', {
+		headers: {
+			Authorization: `token ${token.access_token}`
+		}
+	}).then((res) => res.json());
+	if (emails.message === 'Bad credentials') {
+		return new Response('The generated token was invalid', { status: 400 });
+	}
 
+	//create a jwt
+	let jwt = sign(
+		{
+			user: user,
+			token: token.access_token,
+			emails: emails
+		},
+		env.JWT_SECRET,
+		{
+			expiresIn: '8h'
+		}
+	);
 
-    throw redirect(303, '/');
+	//set the cookie
+	cookies.set('jwt', jwt, {
+		httpOnly: true,
+		path: '/',
+		maxAge: 60 * 60 * 8
+	});
+	cookies.set('welcome', true, {
+		httpOnly: false,
+		path: '/',
+		maxAge: 60 * 60 * 8
+	});
+
+	if (url.searchParams.get('next')) throw redirect(303, url.searchParams.get('next'));
+	else throw redirect(303, '/');
 }
