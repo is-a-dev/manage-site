525176a6ebea3620aa1b4609911c70422b8fed7d
andrewstech <info@andrewstech.me>
2023-08-20 16:58:29 +0000

    export

--- src/routes/domains/[domain]/+page.svelte
+++ src/routes/domains/[domain]/+page.svelte
@@ -68,9 +68,9 @@
 >
 	Manage files
 </button>
-<button class="btn variant-filled" on:click={() => {
-	window.open("https://hosts.is-a.dev/api/download?domain=${data.domain.name}&jwt=${data.jwt}");
-}}>Export Data</button>
+<button class="btn variant-filled mb-2" on:click={() => window.open(`https://hosts.is-a.dev/api/download?domain=${data.domain.name}&jwt=${data.jwt}`)}>
+	export Data
+</button>
 {/if}
 <br />
 <button class="btn variant-filled mb-2" on:click={() => goto(`/domains/${data.domain.name}/edit`)}>
