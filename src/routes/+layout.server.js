393d23b5c7c29f91c7d8b8115700314c754daffa
andrewstech <info@andrewstech.me>
2023-09-01 19:04:49 +0000

    fix

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -2,11 +2,13 @@ import { getJWT } from '$lib/jwt.js'
 import { Maintainer } from '$lib/api.js'
 export async function load({cookies}){
     let user = await getJWT(cookies.get('jwt'));
-    let Maintainers = await Maintainer(user.user.login);
     let maintainer = null;
-    //if Maintainers is true then return false
-    if(!Maintainers) maintainer = false;
-    else maintainer = true;
+    if(user) {
+        let Maintainers = await Maintainer(user.user.login);
+        //if Maintainers is true then return false
+        if(!Maintainers) maintainer = false;
+        else maintainer = true;
+    }
 
     return {
         user,
