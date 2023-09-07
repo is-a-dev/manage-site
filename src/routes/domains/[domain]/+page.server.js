f898013004c4d5649207b60aeba852d6ab536db8
andrewstech <info@andrewstech.me>
2023-09-07 22:16:02 +0000

    .toLowerCase()

--- src/routes/domains/[domain]/+page.server.js
+++ src/routes/domains/[domain]/+page.server.js
@@ -9,7 +9,7 @@ export async function load({cookies, params}){
     let hosting = await getHosting(jwt, params.domain)
     let domain = await DomainInfo(params.domain);
     if(domain.error) throw redirect(303, '/domains');
-    if(domain.owner.username !== user.user.login) throw redirect(303, '/domains')
+    if(domain.owner.username.toLowerCase() !== user.user.login.toLowerCase()) throw redirect(303, '/domains')
     return {
         user: user.user,
         domain: {
